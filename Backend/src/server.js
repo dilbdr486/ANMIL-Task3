304dbf7f70c011b786dd71df4d917eaea75db2f0
Dil Bahadur Tharu <dtharu486@gmail.com>
2025-02-05 15:17:12 +0545

    update with session

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -4,12 +4,23 @@ import cors from "cors"
 import connectDB from "./config/db.js"
 import "dotenv/config"
 import router from "./routes/userRoute.js"
+import session from "express-session"
 
 const port = process.env.PORT || 3001
 const app = express()
+
+const allowedOrigins = ['http://localhost:5173'];
+
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({credentials:true}))
+app.use(cors({origin:allowedOrigins,credentials:true}))
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: process.env.NODE_ENV === 'production' }
+}))
 
 connectDB();
 
